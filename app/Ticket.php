6336b386327ac54fed05deba572487cbef7decb3
Martijn9601 <43539448+Martijn9601@users.noreply.github.com>
2018-11-16 09:10:25 +0100

    added ticket controller

--- app/Ticket.php
+++ app/Ticket.php
@@ -3,13 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Ticket extends Model
-{  		
-  		protected $fillable=[
-  		'user_id', 'category_id', 'ticket_id', 'title', 'priority', 'message', 'status'];
-
-  		public function tickets(){
-  			return $this->hasMany(Ticket::class);
-  		}
+{
+	/**
+	 * 
+	 * 
+	 * @var  array
+	 */
+    protected $fillable = [
+    	'user_id', 'category_id', 'ticket_id', 'title', 'priority', 'message', 'status'
+    ];
+   
+	public function user()
+    {
+    	return $this->belongsTo(User::class);
+   }
+   
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+ 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
